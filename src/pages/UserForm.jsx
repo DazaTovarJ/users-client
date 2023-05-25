7f02b22472cd2d3b3508887a9999b743c3125162
Jaime Daza <jaimedavid250701@outlook.com>
2023-05-24 22:41:13 -0500

    feat(form): Add form fields and button

--- src/pages/UserForm.jsx
+++ src/pages/UserForm.jsx
@@ -1,7 +1,44 @@
 import React from "react";
+import {Formik} from "formik";
+import {Button, Form} from "react-bootstrap";
 
 function UserForm() {
-  return <div>Form</div>;
+  return (
+    <>
+      <h2>New User</h2>
+      <Formik>
+        <Form>
+          <Form.Group controlId="given-name" className="mb-3">
+            <Form.Label>Given Name:</Form.Label>
+            <Form.Control
+              type="text"
+              name="nombre"
+              placeholder="Enter your name"
+            />
+          </Form.Group>
+          <Form.Group controlId="family-name" className="mb-3">
+            <Form.Label>Family Name:</Form.Label>
+            <Form.Control
+              type="text"
+              name="apellido"
+              placeholder="Enter your family name"
+            />
+          </Form.Group>
+          <Form.Group controlId="address" className="mb-3">
+            <Form.Label>Address:</Form.Label>
+            <Form.Control
+              type="text"
+              name="direccion"
+              placeholder="Enter your address"
+            />
+          </Form.Group>
+          <div className="d-grid gap-2">
+            <Button variant="outline-dark">Save</Button>
+          </div>
+        </Form>
+      </Formik>
+    </>
+  );
 }
 
 export default UserForm;
